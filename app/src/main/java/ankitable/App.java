88687b0c9897b74b2b4b1c53308ffde8e07bfa5c
Dan Lewis <20071+danlewis783@users.noreply.github.com>
2024-01-06 11:40:55 -0600

    add support for running multiple CSV's at once (-batch), derive the title from the filename

--- app/src/main/java/ankitable/App.java
+++ app/src/main/java/ankitable/App.java
@@ -29,8 +29,12 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     private static final CSVFormat CSV_FORMAT = CSVFormat.DEFAULT.builder().setQuoteMode(QuoteMode.MINIMAL)
@@ -55,25 +59,86 @@ public class App {
 
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            System.err.println("usage: AnkiTable path/to/file.csv path/to/outputFile.html");
+        if (args.length < 2) {
+            System.err.println(
+                    "usage: AnkiTable path/to/file.csv path/to/outputFile.html\n" +
+                    "                --OR--\n" +
+                    "       AnkiTable -batch directory/containing/csvfiles");
             return;
         }
-        Path csvFile = Paths.get(args[0]);
-        Path outputFile = Paths.get(args[1]);
+        System.out.println(Arrays.toString(args));
+        if (args[0].equals("-batch")) {
+            batch(args[1]);
+        } else {
+            extracted(args[0], args[1]);
+        }
+    }
+
+
+    private static void batch(String csvFilesDirStr) throws IOException {
+        Path csvFilesDir = Paths.get(csvFilesDirStr);
+        batch(csvFilesDir);
+    }
+
+    private static void batch(Path csvFilesDir) throws IOException {
+        if (! Files.exists(csvFilesDir)) {
+            throw new RuntimeException("does not exist");
+        }
+        if (! Files.isDirectory(csvFilesDir)) {
+            throw new RuntimeException("not a directory");
+        }
+        Set<Path> csvFiles;
+        try (Stream<Path> stream = Files.list(csvFilesDir)) {
+            csvFiles = stream.filter(file -> !Files.isDirectory(file)).filter(file -> file.getFileName().toString().endsWith(".csv")).collect(Collectors.toSet());
+        }
+        for (Path file : csvFiles) {
+            Path outputFile = deriveOutputFileFrom(file);
+            extracted(file, outputFile);
+        }
+    }
+
+    private static Path deriveOutputFileFrom(Path file) {
+        String outputFileName = file.getFileName().toString().replace(".csv", ".html");
+        Path outputDir = file.getParent();
+        return outputDir.resolve(outputFileName);
+    }
+
+    private static void extracted(String csvFileName, String outputFileName) throws IOException {
+        Path csvFile = Paths.get(csvFileName);
+        Path outputFile = Paths.get(outputFileName);
+        extracted(csvFile, outputFile);
+    }
+
+    private static void extracted(Path csvFile, Path outputFile) throws IOException {
+        String title = deriveTitle(outputFile);
+        extracted(csvFile, outputFile, title);
+    }
+
+    private static String deriveTitle(Path outputFile) {
+        return fileNameWithoutExtension(outputFile);
+    }
+
+    private static String fileNameWithoutExtension(Path outputFile) {
+        String fileName = outputFile.getFileName().toString();
+        String noExtension = fileName.substring(0, fileName.indexOf(".html"));
+        return noExtension;
+    }
+
+    private static void extracted(Path csvFile, Path outputFile, String title) throws IOException {
+        System.out.println("csvFile = " + csvFile + ", outputFile = " + outputFile + ", title = " + title);
         List<CSVRecord> records;
         try (BufferedReader reader = Files.newBufferedReader(csvFile)) {
             CSVParser parser = CSV_FORMAT.parse(reader);
             records = parser.getRecords();
         }
 
-        int numColumns = numColumns(records).orElseThrow(() -> new IllegalArgumentException("inconsistent number of columns"));
+        int numColumns = numColumns(records).orElseThrow(() -> new IllegalArgumentException("inconsistent number of columns for file " + csvFile));
 
         try (BufferedWriter writer = Files.newBufferedWriter(outputFile);
              PrintWriter printWriter = new PrintWriter(writer)) {
 
             printWriter.println(STYLE);
-            printWriter.println("<div>title</div>\n");
+            printWriter.println("<div>" + title + "</div>\n");
             printWriter.println("<table class=\"fred\">\n");
             printWriter.println("<tr>\n");
             CSVRecord heading = records.get(0);
